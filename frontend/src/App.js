f718ed1a67715eb032e1a912aa7a122cd2829f81
prabhatkumarjena16 <prabhatkumarjena16@gmail.com>
2022-02-19 16:11:04 +0530

    / router component render fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import HomePage from "./screens/HomePage";
 import LoginScreen from "./screens/Login";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Redirect } from "react-router";
 import Header from "./components/Header";
 import withError from "./hooks/withError";
+import { useEffect } from "react";
 
 function App() {
   // Error handling
@@ -13,6 +15,7 @@ function App() {
     <Router>
       <Header />
       <div className="main py-3">
+        <Route exact path="/" render={() => <Redirect to="/home" />} />
         <Route exact path="/home" component={HomeScreen} />
         <Route exact exact path="/login" component={Login} />
       </div>
