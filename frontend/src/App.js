a47d063b03019a2e138c52fb581abcb84fe96090
prabhatkumarjena16 <prabhatkumarjena16@gmail.com>
2022-02-19 16:25:34 +0530

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,27 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Redirect } from "react-router";
 import Header from "./components/Header";
 import withError from "./hooks/withError";
-import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
   // Error handling
   const HomeScreen = withError(HomePage);
   const Login = withError(LoginScreen);
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   return (
     <Router>
       <Header />
       <div className="main py-3">
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route
+          exact
+          path="/"
+          render={() =>
+            userInfo ? <Redirect to="/home" /> : <Redirect to="/login" />
+          }
+        />
         <Route exact path="/home" component={HomeScreen} />
         <Route exact exact path="/login" component={Login} />
       </div>
