c9c4393cf658d9d04e74fd97d44efb11582c9163
prabhatkumarjena16 <prabhatkumarjena16@gmail.com>
2022-02-19 15:49:41 +0530

    issue fix

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
